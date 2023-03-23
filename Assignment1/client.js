5b48a264d2990e7a82cff3734d5914cc2055d8b0
adityakgithub <adityakorade@gmail.com>
2023-03-24 00:42:49 +0530

    TypeScript file added.

---
+++ Assignment1/client.js
@@ -0,0 +1,47 @@
+/*1. Write a typescript program which contains one function named as Maximum. That function accepts
+three parameters and it should returns largest value from three input parameters.
+Input : 23 89 6
+Output : Maximum number is 89
+
+pop
+function maximum(a:number,b:number,c:number):number
+{
+    var res:number=0;
+    a>=b?(a>=c? res=a : res=c):(b>=c?res=b: res=c);
+    return res;
+}*/
+/*
+var no1,no2,no3,iRet:number;
+no1=23,no2=89,no3=6;
+iRet=Math.max(no1,no2,no3);
+console.log("Maximum of 23,89,6 is : ",iRet);*/
+/*
+2. Write a typescript program which contains one function named as Area. That function should
+calculate area of circle. Accept value of radius from user and return its area. Default value of PI
+should be 3.14 if it is not provided by the caller.
+Input : 5
+Output : Area of circle is 78.5
+
+
+function Area(radius:number): number
+{
+    return (Math.PI)*(Math.pow(radius,2));
+}
+var ra:number=5;
+console.log("Area= "+Area(ra)+"("+Math.round(Area(ra))+")cm square");
+*/
+/*
+3. Write a typescript program which contains one function named as DisplayFactors. That function
+should accept one number and display factors of that number.
+Input : 20
+Output : 1 2 4 5 10
+*/
+function DisplayFactors(no) {
+    console.log("Factor of 20= 1 ");
+    for (var i = 2; i < no; i++) {
+        if (no % i == 0)
+            console.log("\t" + i + "\t");
+    }
+}
+var ra = 20;
+DisplayFactors(ra);
---
+++ Assignment2/client.js
@@ -0,0 +1,187 @@
+/* 1. Write a typescript program which contains one function named as Maximum. That function accepts
+array of numbers and returns the largest number from array.
+Input : 23 89 6 29 56 45 77 32
+Output : Maximum number is 89
+
+pop
+function maximum(arr:number[]):number
+{
+    var iCnt: number=0;
+    var max: number =arr[0];
+    for(iCnt ; iCnt<arr.length; iCnt++)
+    {
+        if(arr[iCnt] > max)
+               max = arr[iCnt];
+    }
+    return max;
+}
+
+var arr:number[]=[23,89,6,29,56,45,77,32];
+var iRet:number=maximum(arr);
+console.log("Maximum in Array: ",iRet);
+______________________________________________________________________________
+oop
+class maximum
+{
+    arr:number[];
+    constructor(arr:number[]) {
+        this.arr=arr;
+    }
+    
+    max()
+    {
+        var iCnt:number=0;
+        var max: number =arr[0];
+        {
+            for(iCnt ; iCnt<arr.length; iCnt++)
+            {
+                if(arr[iCnt] > max)
+                    max = arr[iCnt];
+            }
+        return max;
+        }
+    }
+    
+}
+
+var arr:number[]=[23,89,6,29,56,45,77,32]
+var obj=new maximum(arr);
+var iRet=obj.max();
+console.log("Maximum in Array: ",iRet);*/
+/* 2. Write a typescript program which contains one function named as Summation. That function
+accepts array of numbers and returns the summation of each number from array.
+Input : 23 6 7 4 5 7
+Output : Addition is 52
+pop
+function summation(arr:number[]):number
+{
+    var iCnt: number=0;
+    var sum: number =0;
+    for(iCnt ; iCnt<arr.length; iCnt++)
+    {
+        sum=sum+arr[iCnt];
+    }
+    return sum;
+}
+
+var arr:number[]=[23,6, 7, 4, 5, 7];
+var iRet:number=summation(arr);
+console.log("Summation of Array: ",iRet);
+______________________________________________________________________________
+oop
+class summation
+{
+    arr:number[];
+    constructor(arr:number[]) {
+        this.arr=arr;
+    }
+    
+    sum()
+    {
+        var iCnt: number =0;
+        var sum : number =0;
+        {
+            for(iCnt ; iCnt<arr.length; iCnt++)
+            {
+                sum=sum+arr[iCnt];
+            }
+        return sum;
+        }
+    }
+    
+}
+
+var arr:number[]=[23,6, 7, 4, 5, 7]
+var obj=new summation(arr);
+var iRet=obj.sum();
+console.log("Maximum in Array: ",iRet);
+*/
+/* 3. Write a typescript program which contains one function named as Maximum. That function accepts
+array of numbers and returns the second largest number from array.
+Input : 23 89 6 29 56 45 77 32
+Output : Second Maximum number is 77
+
+function maximum(arr:number[]):number
+{
+    var iCnt: number=0;
+    var max: number =arr[0];
+    var s2ndH: number=0;
+    for(iCnt ; iCnt<arr.length; iCnt++)
+    {
+        if(arr[iCnt] > max)
+               max = arr[iCnt];
+    }
+    for (iCnt=0;iCnt<arr.length; iCnt++)
+    {
+        if (arr[iCnt]<max && arr[iCnt]>=s2ndH ) {
+            s2ndH=arr[iCnt]
+        }
+    }
+    return s2ndH;
+}
+
+var arr:number[]=[23,89,6,29,56,45,77,32];
+var iRet:number=maximum(arr);
+console.log("2nd Maximum in Array: ",iRet);*/
+/* 4. Write a typescript program which contains one arrow function named as ChkArmstrong. That
+function accepts one numbers and check whether number is Armstrong number or not.
+Input : 153
+Output : It is Armstrong number
+function ChkArmstrong(fNo:number)
+{
+    var fNoTemp : number = fNo;
+    var sum : number = 0, digits: number=0, last:number=0 ;
+    while(fNoTemp>0)
+    {
+        fNoTemp = fNoTemp/10;
+        digits++;
+    }
+    fNoTemp=fNo;
+    while(fNoTemp>0)
+    {
+        last = fNoTemp % 10;
+        sum +=  (Math.pow(last, digits));
+        fNoTemp = fNoTemp/10;
+    }
+
+    if(fNo==sum)
+    return true;
+    return false;
+
+}
+
+var aNo : number =  153;
+var iRt : boolean = ChkArmstrong(aNo);
+if (iRt!= true)
+{
+    console.log("Number "+aNo+" is Armstrong!");
+} else
+{
+    console.log("Number "+aNo+" is Not Armstrong!");
+}
+*/
+/* 5. Write a typescript program which contains one function named as ChkString. That function accept
+one string and check whether that string contains “Marvellous” word or not.
+Input : “Pune Kothrud Marvellous Infosystems”
+Output : String contains Marvellous in it. */
+function ChkString(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+    if (s1.indexOf(s2) >= 0) {
+        console.log("value: ", s1.indexOf(s2));
+        return true;
+    }
+    else {
+        console.log("Value: ", s1.indexOf(s2));
+        return false;
+    }
+}
+var oStr = "Pune Kothrud Marvellous Infosystems";
+var fStr = "SYSTEM";
+var iRt = ChkString(oStr, fStr);
+if (iRt != false) {
+    console.log("string: \"" + oStr + "\" include \"" + fStr + "\"");
+}
+else {
+    console.log("string: \"" + oStr + "\" do not include \"" + fStr + "\"");
+}
